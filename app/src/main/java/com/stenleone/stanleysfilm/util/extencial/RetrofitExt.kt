c6a0b81e3476c9b87c56f35e3c81e9cee9c756a9
stenleone <stenleone@gmail.com>
2021-01-15 23:55:20 +0200

    small review

--- app/src/main/java/com/stenleone/stanleysfilm/util/extencial/RetrofitExt.kt
+++ app/src/main/java/com/stenleone/stanleysfilm/util/extencial/RetrofitExt.kt
@@ -4,8 +4,9 @@ import retrofit2.Response
 
 fun <T> Response<T>.successOrError(success: (T) -> Unit, error: (String) -> Unit) {
     if (this.isSuccessful) {
-        if (body() != null) {
-            body()?.let { success(it) }
+        val body = body()
+        if (body != null) {
+            success(body)
         } else {
             error(message())
         }
